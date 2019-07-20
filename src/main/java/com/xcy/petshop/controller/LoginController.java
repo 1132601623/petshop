724838b88ef3095fd03d53a77cc10e64dcbcecb6
wuyunfei <1132601623@qq.com>
2019-07-20 11:20:04 +0800

    2019-7-20 11:11上传  评论功能完成 登录注册完成 首页展示数据修改

--- src/main/java/com/xcy/petshop/controller/LoginController.java
+++ src/main/java/com/xcy/petshop/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.xcy.petshop.controller;
 
 import com.xcy.petshop.pojo.User;
 import com.xcy.petshop.service.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class LoginController {
   @Autowired UserService userService;
 
   @RequestMapping("/validateEmail")
+  @ApiOperation("注册用户的时候，先去数据库查询邮箱是否存在，如果存在，则无法进行注册")
   public String validateEmail(String email, HttpServletResponse response) {
     response.setHeader("Access-Control-Allow-Origin", "*");
 
@@ -27,6 +29,7 @@ public class LoginController {
   }
 
   @RequestMapping("/registerEmail")
+  @ApiOperation("用户注册")
   public String registerEmail(User user, HttpServletResponse response) {
     response.setHeader("Access-Control-Allow-Origin", "*");
     userService.registerEmail(user);
@@ -34,6 +37,7 @@ public class LoginController {
   }
 
   @RequestMapping("/login")
+  @ApiOperation("用户登录")
   public String login(User user, HttpServletResponse response) {
     response.setHeader("Access-Control-Allow-Origin", "*");
     boolean result = userService.isLogin(user);
