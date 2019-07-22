3d4c77b22458eccda5b58418211ce32b1a660440
wuyunfei <1132601623@qq.com>
2019-07-22 21:46:26 +0800

    2019-7-22 21:46上传 评论功能完成 登录注册完成 首页展示数据修改 附近条件查询完成 猫粮、狗粮展示完毕

--- src/main/java/com/xcy/petshop/mapper/SelectPetMapper.java
+++ src/main/java/com/xcy/petshop/mapper/SelectPetMapper.java
@@ -1,13 +1,12 @@
 package com.xcy.petshop.mapper;
 
-
 import com.xcy.petshop.pojo.Pet;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
+
 @Mapper
 public interface SelectPetMapper {
-    List<Pet> selectAllCatById(int id);
 
-    List<Pet> selectAllDogById(int id);
+  List<Pet> selectAllPetById(int id);
 }
